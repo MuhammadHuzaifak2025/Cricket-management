8018bb9d4be0da208efc0d8956e32f338d12336d
Muhammad Huzaifa <k224337@nu.edu.pk>
2024-03-27 15:30:46 +0000

    testing

--- GullySports/GullySports/urls.py
+++ GullySports/GullySports/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from views.py import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("createuser/", ),
 ]
--- GullySports/sports/views.py
+++ GullySports/sports/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from models import *
+from django.contrib.auth.models import User,create_user
 
-# Create your views here.
+def Create_user(username, password, email):
+    username = "Huzaifa"
+    password = "check"
+    email = None
+    create_user(username, email, password)
+
