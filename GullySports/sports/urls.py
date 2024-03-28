7a94240de99b5cd67404e429fd6a95bb53e12411
Muhammad Huzaifa <k224337@nu.edu.pk>
2024-03-28 15:28:06 +0000

    Code Updated of user models

--- GullySports/sports/urls.py
+++ GullySports/sports/urls.py
@@ -1,9 +1,7 @@
-
 from django.contrib import admin
 from django.urls import path, include
-
+from sports import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', include('sports.urls'))
+    path('', views.Create_user, name="CreateUser")
 ]
--- GullySports/sports/views.py
+++ GullySports/sports/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from models import *
-from django.contrib.auth.models import User,create_user
+from .models import Innings
+from django.contrib.auth.models import User
+from django.http import JsonResponse
 
-def Create_user(username, password, email):
-    username = "Huzaifa"
+def Create_user(request, username=None, email=None, password=None):
+    username = "Huzaifa_Ali3"
     password = "check"
     email = None
-    create_user(username, email, password)
-
+    if not User.objects.filter(username=username):
+        User.objects.create_user(username=username, email=email, password=password)
+        return JsonResponse({"User":"created"})
+    return JsonResponse({"User":"exsist"})
